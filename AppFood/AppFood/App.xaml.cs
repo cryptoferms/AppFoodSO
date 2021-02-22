3e7d5ec6e2081accabe68262e13a9a9609cf2dc7
fernando Schilipack <nando_fernendo@hotmail.com>
2021-02-22 14:21:48 -0300

    Adicionado a pag Login

--- AppFood/AppFood/App.xaml.cs
+++ AppFood/AppFood/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppFood.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace AppFood
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginViewPage();
         }
 
         protected override void OnStart()
