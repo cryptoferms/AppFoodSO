06f3997209856603fa8c20769df65b86169cdf5c
fernando Schilipack <nando_fernendo@hotmail.com>
2021-02-23 14:04:08 -0300

    criado a base para o MVVM

---
+++ AppFood/AppFood/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFood.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel()
+        {
+
+        }
+    }
+}
