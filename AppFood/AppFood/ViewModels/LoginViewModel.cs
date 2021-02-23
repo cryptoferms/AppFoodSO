a7262c7d0156c4544ac7989dfb71105f548aab44
fernando Schilipack <nando_fernendo@hotmail.com>
2021-02-23 14:29:54 -0300

    criando loginviewmodel

--- AppFood/AppFood/ViewModels/LoginViewModel.cs
+++ AppFood/AppFood/ViewModels/LoginViewModel.cs
@@ -6,6 +6,28 @@ namespace AppFood.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private string _Username;
+
+        public string Username
+        {
+            get { return _Username; }
+            set { _Username = value; OnPropertyChanged(); }
+        }
+        private string _Password;
+
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; OnPropertyChanged(); }
+        }
+        private bool _isBusy;
+
+        public bool isBusy
+        {
+            get { return _isBusy; }
+            set { _isBusy = value; OnPropertyChanged(); }
+        }
+
         public LoginViewModel()
         {
 
